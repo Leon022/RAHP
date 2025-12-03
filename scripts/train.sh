5d15f6331908d70c6f598d0ac4c96344c3ca5d49
Leon022 <97142723+Leon022@users.noreply.github.com>
2025-12-03 12:05:30 +0800

    Update train.sh

--- scripts/train.sh
+++ scripts/train.sh
@@ -2,7 +2,8 @@
 
 SCRIPT_PATH=$(readlink -f "$0")
 SCRIPT_DIR=$(dirname "$SCRIPT_PATH")
-export DATASET_DIR="${SCRIPT_DIR}/../DATASET"
+export DATASET_DIR="$(realpath "${SCRIPT_DIR}/../DATASET")"
+echo ${DATASET_DIR}
 
 export CUDA_VISIBLE_DEVICES=0
 export NUM_GPUS=4
@@ -18,12 +19,12 @@ cp ./maskrcnn_benchmark/modeling/rpn/vldyhead.py ./OUTPUT/${MODEL_NAME}/
 cp ./maskrcnn_benchmark/modeling/relation_head/ov_classifier.py ./OUTPUT/${MODEL_NAME}
 cp ./scripts/train.sh ./OUTPUT/${MODEL_NAME}/
 
-python -m torch.distributed.launch \
-    --master_port 8888 --nproc_per_node=${NUM_GPUS} \
+python3 -m torch.distributed.launch \
+    --master_port 8886 --nproc_per_node=${NUM_GPUS} \
     tools/train_net.py \
     --task_config configs/vg150/finetune.yaml --config-file configs/pretrain/glip_Swin_T_O365_GoldG.yaml \
-    SOLVER.IMS_PER_BATCH $(( 1*${NUM_GPUS} )) \
-    TEST.IMS_PER_BATCH $(( 1*${NUM_GPUS}  )) \
+    SOLVER.IMS_PER_BATCH $(( 8*${NUM_GPUS} )) \
+    TEST.IMS_PER_BATCH $(( 8*${NUM_GPUS}  )) \
     MODEL.DYHEAD.RELATION_REP_REFINER False \
     MODEL.ROI_RELATION_HEAD.PREDICT_USE_BIAS False \
     MODEL.ROI_RELATION_HEAD.USE_GT_BOX False \
@@ -31,6 +32,7 @@ python -m torch.distributed.launch \
     SOLVER.VAL_PERIOD 6000 \
     SOLVER.CHECKPOINT_PERIOD 3000 \
     DATASETS.VG150_OPEN_VOCAB_MODE False \
-    OUTPUT_DIR ./OUTPUT/${MODEL_NAME}
+    OUTPUT_DIR ./OUTPUT/${MODEL_NAME} \
+    MODEL.DYHEAD.OV.DYNAMIC_CLIP_CLASSIFIER_WEIGHT_CACHE_PTH ../DATASET/VG150/VG150_relation_aware_embedding.pt
 
 
